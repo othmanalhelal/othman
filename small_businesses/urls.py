f3e37070fc03bda722504c57ca645ddb3ad6734c
GitHubUsername <othmanalhelal@Othmans-MacBook-Pro.local>
2017-11-26 17:45:53 +0300

    Bootstap setup

--- small_businesses/urls.py
+++ small_businesses/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -22,4 +25,6 @@ url(r'^admin/', admin.site.urls),
 url(r'^businesses/', include('businesses.urls', namespace="businesses")),
 
 ]
-
+if settings.DEBUG:
+    urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
