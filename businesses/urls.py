7f10a276c883ee55bd7500cc1dc4beeaba04832f
GitHubUsername <othmanalhelal@Othmans-MacBook-Pro.local>
2017-11-26 08:58:45 +0300

    Delete views

--- businesses/urls.py
+++ businesses/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     url(r'^detail/(?P<business_id>\d+)/$', views.business_detail, name="detail"),
     url(r'^list/$', views.business_list, name="list"),
     url(r'^update/(?P<business_id>\d+)/$', views.business_update, name="update"),
-    url(r'^delete/$', views.business_delete, name="delete"),
     url(r'^delete/(?P<business_id>\d+)/$', views.business_delete, name="delete"),
     ]
